6a119f797132c146a71d48c8043bd7a9fd4ca84a
taniashahida-dev <taniashahida993@gmail.com>
2026-05-11 10:19:43 +0600

    Make hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Banner from "@/components/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
-    Home page
+   <Banner></Banner>
    </div>
   );
 }
